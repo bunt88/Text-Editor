879615168b6834ec2c0b291c05912fd8168ea6ce
unknown <bunt88@gmail.com>
2022-09-21 17:50:21 -0600

    Fiexed Error in webpack

--- _Develop/client/webpack.config.js
+++ _Develop/client/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: "Just Another Text Editor",
-        short_name "JATE",
+        short_name: "JATE",
         description: "Text Editor with Offline Capabilities Using IndexedDB",
         background_color: "#225ca3",
         theme_color: "#225ca3",
